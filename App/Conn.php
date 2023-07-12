de72303ada474583b2d22bb66ca1294ebb8bc4ba
Marcelo Coelho <macj.10@hotmail.com>
2023-07-11 21:46:49 -0300

    Singleton

--- App/Conn.php
+++ App/Conn.php
@@ -7,25 +7,39 @@ use PDOException;
 
 class Conn
 {
+    private static ?PDO $instance = null;
 
-    protected $host = "127.0.0.1";
-    protected $user = "root";
-    protected $pass = "";
-    protected $dbname = "rbaromas";
-    protected $pdo = "false";
-
-    public function connection()
+    private function __construct()
     {
+        //private construct
+    }
+
+    public static function getInstance(): PDO
+    {
+        if (self::$instance === null) {
+            self::$instance = self::createConnection();
+        }
+
+        return self::$instance;
+    }
+
+    private static function createConnection(): PDO
+    {
+        $envFile = __DIR__ . '/../.env';
+        $envVars = parse_ini_file($envFile);
+        $host = $envVars["DB_HOST"];
+        $user = $envVars["DB_USER"];
+        $pass = $envVars["DB_PASSWORD"];
+        $dbName = $envVars["DB_DATABASE"];
         try {
-            #conectando ao banco de dados
-            $this->pdo = new PDO("mysql:host=$this->host; dbname=$this->dbname", $this->user, $this->pass);
-            #definindo a utilização de report de erros do tipo Exception
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            #definindo o padrão de condificação
-            $this->pdo->exec("SET NAMES utf8");
+            $pdo = new PDO("mysql:host=$host;dbname=$dbName", $user, $pass);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $pdo->exec("SET NAMES utf8");
         } catch (PDOException $e) {
             echo "Erro: " . $e->getMessage();
+            exit();
         }
-        return $this->pdo;
+
+        return $pdo;
     }
 }
