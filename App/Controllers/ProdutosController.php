351b377608a20b5f01b4103a1e6926cb05f8d225
Marcelo Coelho <macj.10@hotmail.com>
2023-07-12 16:22:18 -0300

    ajustes produtos

--- App/Controllers/ProdutosController.php
+++ App/Controllers/ProdutosController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\Produto;
+use App\Service\ProductService;
 use Config\Controller\Action;
 
 class ProdutosController extends Action
@@ -13,120 +14,51 @@ class ProdutosController extends Action
 
     public function index()
     {
-        $produto = new Produto();
-        $this->dados = $produto->readProduto();
-        $this->render("Produto/index.phtml", "layoutAuth");
+        $productService = new ProductService();
+        $productService->productsView();
     }
 
 
     public function cadastroproduto()
     {
-        if ($_SESSION['sId'] == "admin") {
-            $this->render("Produto/cadastro.phtml", "layoutAdmin");
-        } else {
-            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">Você não possui permissão para acessar está página!</div>";
-            $this->render("Auth/login.phtml", "layoutAuth");
-        }
+        $productService = new ProductService();
+        $productService->registerView();
     }
 
     public function registrar()
     {
-
-        $produto = new Produto();
-        $produto->__set("nome", $_POST['nome']);
-        $produto->__set("categoria", $_POST['categoria']);
-        $produto->__set("descricao", $_POST['descricao']);
-        $produto->__set("img", $_POST['img']);
-        $produto->__set("EstoqueQtd", $_POST['EstoqueQtd']);
-        $produto->__set("preco", $_POST['preco']);
-
-
-        if ($produto->validarCadastro()) {
-            $produto->createProduto();
-        } else {
-            $this->cad['formRetorno'] = $_POST;
-        }
-
-        $this->render("Produto/cadastro.phtml", "layoutAdmin");
+        $productService = new ProductService();
+        $productService->register();
     }
 
     public function listaProdutos()
     {
-        if (isset($_SESSION['sId']) && isset($_SESSION['sNome'])) {
-            if ($_SESSION['sId'] == "admin") {
-                $produto = new Produto();
-                $this->dados = $produto->readProduto();
-                $this->render("Produto/listaProduto.phtml", "layoutAdmin");
-            } else {
-                $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">Você não possui permissão para acessar está página!</div>";
-                $this->render("Auth/login.phtml", "layoutAuth");
-            }
-        } else {
-            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">Você não possui permissão para acessar está página!</div>";
-            $this->render("Auth/login.phtml", "layoutAuth");
-        }
+        $productService = new ProductService();
+        $productService->listProducts();
     }
 
     public function delete()
     {
-        if (isset($_SESSION['sId']) && isset($_SESSION['sNome'])) {
-            if (isset($_POST['id'])) {
-                $produto = new Produto();
-                $produto->__set('CodProduto', $_POST['id']);
-                echo $_POST['id'];
-                $produto->excluirProduto();
-                header("listaprodutos");
-                $_SESSION['msg'] = "<div class=\"alert alert-success\" role=\"alert\">Produto excluido com sucesso!</div>";
-            } else {
-                $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">ID do produto não informado!</div>";
-            }
-            header("location: /listaprodutos");
-        } else {
-            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">Você não possui permissão para acessar está página!</div>";
-            $this->render("Auth/login.phtml", "layoutAuth");
-        }
+        $productService = new ProductService();
+        $productService->delete();
     }
 
 
     public function admExecAtualizarProduto()
     {
-        if (isset($_SESSION['sId']) && isset($_SESSION['sNome'])) {
-            if ($_SESSION['sId'] == "admin") {
-                $produto = new Produto();
-                $produto->__set("id", $_POST['id']);
-                $this->dados = $produto->selecionarProduto();
-                $this->render("Produto/atualizar.phtml", "layoutAdmin");
-            }
-        } else {
-            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">Você não possui permissão para acessar está página!</div>";
-            $this->render("Auth/login.phtml", "layoutAuth");
-        }
+        $productService = new ProductService();
+        $productService->updateViewAdmin();
     }
 
     public function execAlterarProduto()
     {
-        if (isset($_SESSION['sId']) && isset($_SESSION['sNome'])) {
-            if ($_SESSION['sId'] == "admin") {
-                $prod = new Produto();
-                echo $_POST['id'];
-                $prod->alterar($_POST['alter'], $_POST['campo'], $_POST['id']);
-                $_SESSION['msg'] = "<div class=\"alert alert-success\" role=\"alert\">Alterado com sucesso!</div>";
-                header("location: /listaprodutos");
-            }
-        } else {
-            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">Você não possui permissão para acessar está página!</div>";
-            $this->render("Auth/login.phtml", "layoutAuth");
-        }
+        $productService = new ProductService();
+        $productService->update();
     }
 
     public function buscar()
     {
-        $produto = new Produto();
-        $this->dados = $produto->buscar($_GET['busca']);
-        if ($this->dados == null) {
-            header("location: /produtos");
-        } else {
-            $this->render("Produto/busca.phtml", "layoutAuth");
-        }
+        $productService = new ProductService();
+        $productService->search();
     }
 }
---
+++ App/Services/ProductService.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\Produto;
+use Config\Controller\Action;
+
+class ProductService extends Action
+{
+
+    protected $dados = null;
+    protected $cad = null;
+
+    /**
+     * List products view
+     *
+     * @return void
+     */
+    public function productsView(): void
+    {
+        $product = new Produto();
+        $this->dados = $product->readProduto();
+        $this->render("Produto/index.phtml", "layoutAuth");
+    }
+
+    /**
+     * Register view product
+     *
+     * @return void
+     */
+    public function registerView(): void
+    {
+        if ($_SESSION['sId'] == "admin") {
+            $this->render("Produto/cadastro.phtml", "layoutAdmin");
+        } else {
+            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">
+                Você não possui permissão para acessar está página!</div>";
+            $this->render("Auth/login.phtml", "layoutAuth");
+        }
+    }
+
+    /**
+     * Register product
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $productModel = new Produto();
+        $productModel->__set("nome", $_POST['nome']);
+        $productModel->__set("categoria", $_POST['categoria']);
+        $productModel->__set("descricao", $_POST['descricao']);
+        $productModel->__set("img", $_POST['img']);
+        $productModel->__set("EstoqueQtd", $_POST['EstoqueQtd']);
+        $productModel->__set("preco", $_POST['preco']);
+        if ($productModel->validarCadastro()) {
+            $productModel->createProduto();
+        } else {
+            $this->cad['formRetorno'] = $_POST;
+        }
+        $this->render("Produto/cadastro.phtml", "layoutAdmin");
+    }
+
+    /**
+     * List products
+     *
+     * @return void
+     */
+    public function listProducts(): void
+    {
+        if (isset($_SESSION['sId']) && isset($_SESSION['sNome'])) {
+            if ($_SESSION['sId'] == "admin") {
+                $productModel = new Produto();
+                $this->dados = $productModel->readProduto();
+                $this->render("Produto/listaProduto.phtml", "layoutAdmin");
+            } else {
+                $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">
+                    Você não possui permissão para acessar está página!</div>";
+                $this->render("Auth/login.phtml", "layoutAuth");
+            }
+        } else {
+            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">
+                Você não possui permissão para acessar está página!</div>";
+            $this->render("Auth/login.phtml", "layoutAuth");
+        }
+    }
+
+    /**
+     * Delete product by id
+     *
+     * @return void
+     */
+    public function delete(): void
+    {
+        if (isset($_SESSION['sId']) && isset($_SESSION['sNome'])) {
+            if (isset($_POST['id'])) {
+                $productModel = new Produto();
+                $productModel->__set('CodProduto', $_POST['id']);
+                echo $_POST['id'];
+                $productModel->excluirProduto();
+                header("listaprodutos");
+                $_SESSION['msg'] = "<div class=\"alert alert-success\" role=\"alert\">
+                    Produto excluido com sucesso!</div>";
+            } else {
+                $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">
+                    ID do produto não informado!</div>";
+            }
+            header("location: /listaprodutos");
+        } else {
+            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">
+                Você não possui permissão para acessar está página!</div>";
+            $this->render("Auth/login.phtml", "layoutAuth");
+        }
+    }
+
+    /**
+     * Update product view admin
+     *
+     * @return void
+     */
+    public function updateViewAdmin(): void
+    {
+        if (isset($_SESSION['sId']) && isset($_SESSION['sNome'])) {
+            if ($_SESSION['sId'] == "admin") {
+                $productModel = new Produto();
+                $productModel->__set("id", $_POST['id']);
+                $this->dados = $productModel->selecionarProduto();
+                $this->render("Produto/atualizar.phtml", "layoutAdmin");
+            }
+        } else {
+            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">
+                Você não possui permissão para acessar está página!</div>";
+            $this->render("Auth/login.phtml", "layoutAuth");
+        }
+    }
+
+    /**
+     * Update product
+     *
+     * @return void
+     */
+    public function update(): void
+    {
+        if (isset($_SESSION['sId']) && isset($_SESSION['sNome'])) {
+            if ($_SESSION['sId'] == "admin") {
+                $productModel = new Produto();
+                echo $_POST['id'];
+                $productModel->alterar($_POST['alter'], $_POST['campo'], $_POST['id']);
+                $_SESSION['msg'] = "<div class=\"alert alert-success\" role=\"alert\">Alterado com sucesso!</div>";
+                header("location: /listaprodutos");
+            }
+        } else {
+            $_SESSION['msg'] = "<div class=\"alert alert-danger\" role=\"alert\">
+                Você não possui permissão para acessar está página!</div>";
+            $this->render("Auth/login.phtml", "layoutAuth");
+        }
+    }
+
+    /**
+     * Search product
+     *
+     * @return void
+     */
+    public function search(): void
+    {
+        $productModel = new Produto();
+        $this->dados = $productModel->buscar($_GET['busca']);
+        if ($this->dados == null) {
+            header("location: /produtos");
+        } else {
+            $this->render("Produto/busca.phtml", "layoutAuth");
+        }
+    }
+}
